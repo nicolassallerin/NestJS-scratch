5aaf4c89d892a7d7eedf9801d5975dc6aceca65a
nicolassallerin <nicolas.sallerin@oclock.io>
2023-02-03 08:29:32 +0100

    test routes

---
+++ src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('/app')
+export class AppController {
+  @Get('/asdf')
+  getRootRoute() {
+    return 'Hello World';
+  }
+  @Get('/bye')
+  getByThere() {
+    return 'Bye there';
+  }
+}
